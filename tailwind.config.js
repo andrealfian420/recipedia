fdc432d23d79481848fe45e051d4416cba633370
andrealfian420 <andrealfian69699@gmail.com>
2020-08-28 17:04:29 +0700

    Adding custom configuration and my own classes for tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FF6363',
+
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        },
+      },
+
+      fontFamily: {
+        body: ['Nunito'],
+      },
+    },
   },
   variants: {},
   plugins: [],
-}
+};
