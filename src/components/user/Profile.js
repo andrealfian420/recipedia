57bfce6f6b3d301d6d9180e1177936fe6a0b984e
andrealfian420 <andrealfian69699@gmail.com>
2020-09-18 23:32:57 +0700

    Changing user navbar behaviour in profile component

--- src/components/user/Profile.js
+++ src/components/user/Profile.js
@@ -1,16 +1,20 @@
 import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 import UserProfileNavbar from '../layout/UserProfileNavbar';
+import SignOutLinks from '../layout/SignOutLinks';
 import UserProfileSection from './UserProfileSection';
 import MyRecipe from '../recipe/MyRecipe';
 
-const Profile = () => {
+const Profile = (props) => {
   useEffect(() => {
     document.title = 'Profile';
   });
 
+  const { auth } = props;
+
   return (
     <main className="px-16 py-6 bg-gray-100 md:col-span-10">
-      <UserProfileNavbar />
+      {auth?.uid ? <UserProfileNavbar /> : <SignOutLinks />}
       <UserProfileSection />
 
       <div className="flex flex-col justify-center md:justify-start p-4">
@@ -24,4 +28,10 @@ const Profile = () => {
   );
 };
 
-export default Profile;
+const mapStateToProps = (state) => {
+  return {
+    auth: state.firebase.auth,
+  };
+};
+
+export default connect(mapStateToProps)(Profile);
