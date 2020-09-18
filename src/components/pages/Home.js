af17c40a056cf1c69d38f69ef0278d2b42094997
andrealfian420 <andrealfian69699@gmail.com>
2020-09-18 16:24:49 +0700

    Changing the behaviour of navbar on home component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,18 +1,22 @@
 import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 
 import UserProfileNavbar from '../layout/UserProfileNavbar';
+import SignOutLinks from '../layout/SignOutLinks';
 import Header from '../layout/Header';
 import LatestRecipes from '../recipe/LatestRecipes';
 import PopularRecipes from '../recipe/PopularRecipes';
 
-const Home = () => {
+const Home = (props) => {
   useEffect(() => {
     document.title = 'Recipedia';
   });
 
+  const { auth } = props;
+
   return (
     <main className="px-16 py-6 bg-gray-100 md:col-span-10">
-      <UserProfileNavbar />
+      {auth?.uid ? <UserProfileNavbar /> : <SignOutLinks />}
       <Header />
       <LatestRecipes />
       <PopularRecipes />
@@ -20,4 +24,10 @@ const Home = () => {
   );
 };
 
-export default Home;
+const mapStateToProps = (state) => {
+  return {
+    auth: state.firebase.auth,
+  };
+};
+
+export default connect(mapStateToProps)(Home);
