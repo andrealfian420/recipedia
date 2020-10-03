778e1100c03407e81878b9188525b50f0c6328e6
andrealfian420 <andrealfian69699@gmail.com>
2020-10-03 21:12:28 +0700

    Changing profileinfo layout to default layout

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
         />
         <ProtectedRoute
           path="/profileinfo"
-          layout={AuthLayout}
+          layout={DefaultLayout}
           component={ProfileInfo}
         />
         <PublicRoute
