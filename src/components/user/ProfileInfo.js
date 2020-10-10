fa7f9c0c6385ade735ea17fcf3c6b636015580e7
andrealfian420 <andrealfian69699@gmail.com>
2020-10-10 15:17:03 +0700

    Separating the profile info component and the update profile component

--- src/components/user/ProfileInfo.js
+++ src/components/user/ProfileInfo.js
@@ -1,174 +1,27 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { updateProfile } from '../../store/actions/authActions';
 import UserProfileNavbar from '../layout/UserProfileNavbar';
+import UpdateProfile from './UpdateProfile';
 
 const ProfileInfo = (props) => {
   useEffect(() => {
     document.title = 'Edit Profile Info';
     props.cleanupUpdateMessage();
-    props.cleanupNewImageUrl();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-  const { profile, userId, updateProfile, successUpdateProfile } = props;
-  const [imageTempURL, setImageTempURL] = useState(null);
-  const [firstName, setFirstName] = useState(null);
-  const [lastName, setLastName] = useState(null);
-  const [newProfileImage, setNewProfileImage] = useState(null);
-  const imageInput = useRef();
-
-  const handleInputClick = () => {
-    return imageInput.current.click();
-  };
-
-  const handleImageInput = (e) => {
-    const inputtedImage = e.target.files[0];
-    const timeStamp = new Date().getTime();
-    const fileName = `${profile.firstName}-${profile.lastName}-${timeStamp}`;
-    setImageTempURL(URL.createObjectURL(inputtedImage));
-
-    const image = {
-      fileName,
-      image: inputtedImage,
-    };
-
-    return setNewProfileImage(image);
-  };
-
-  const handleFirstNameChange = (e) => {
-    setFirstName(e.target.value);
-  };
-
-  const handleLastNameChange = (e) => {
-    setLastName(e.target.value);
-  };
-
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    const newData = {
-      newProfileImage: newProfileImage,
-      firstName: firstName ?? profile.firstName,
-      lastName: lastName ?? profile.lastName,
-    };
-
-    return updateProfile(userId, newData);
-  };
-
-  if (!profile) {
-    return <div>Loading...</div>;
-  }
 
   return (
     <main className="px-16 py-6 bg-gray-100 md:col-span-10">
       <UserProfileNavbar />
-
-      <div className="w-full md:w-3/4 py-4 px-2 md:mx-auto bg-white shadow-md md:shadow-lg rounded mt-4 md:mt-8">
-        <form className="p-2" onSubmit={handleFormSubmit}>
-          <div className="relative rounded-full w-64 h-64 mx-auto overflow-hidden">
-            <img
-              src={imageTempURL ?? profile.profileImageUrl}
-              alt="Profile"
-              className="object-cover w-full h-full"
-            />
-            <span
-              className="absolute w-full  py-3 rounded-sm text-center bg-black text-white opacity-50 hover:opacity-75 cursor-pointer"
-              style={{ top: '80%' }}
-              onClick={handleInputClick}
-            >
-              Change Photo
-            </span>
-            <input
-              type="file"
-              ref={imageInput}
-              className="hidden"
-              accept="image/png,image/jpg,image/jpeg"
-              onChange={handleImageInput}
-            />
-          </div>
-          <div className="flex flex-col px-4 mt-6">
-            <label
-              htmlFor="email"
-              className="inline-block mb-1 md:mb-0 text-center md:text-left text-xl font-semibold"
-            >
-              Email Address
-            </label>
-            <input
-              type="text"
-              id="email"
-              className="shadow bg-gray-300 opacity-75 appearance-none border rounded w-full md:w-3/4 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="Write your first name here..."
-              defaultValue={profile.email}
-              disabled
-            />
-          </div>
-          <div className="flex flex-col px-4 mt-4">
-            <label
-              htmlFor="firstName"
-              className="inline-block mb-1 md:mb-0 text-center md:text-left text-xl font-semibold"
-            >
-              First Name
-            </label>
-            <input
-              type="text"
-              id="firstName"
-              className="shadow appearance-none border rounded w-full md:w-3/4 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="Write your first name here..."
-              defaultValue={firstName ?? profile.firstName}
-              onChange={handleFirstNameChange}
-            />
-          </div>
-          <div className="flex flex-col px-4 mt-4">
-            <label
-              htmlFor="lastName"
-              className="inline-block mb-1 md:mb-0 text-center md:text-left text-xl font-semibold"
-            >
-              Last Name
-            </label>
-            <input
-              type="text"
-              id="lastName"
-              className="shadow appearance-none border rounded w-full md:w-3/4 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="Write your last name here..."
-              defaultValue={lastName ?? profile.lastName}
-              onChange={handleLastNameChange}
-            />
-          </div>
-
-          <div className="flex flex-col justify-center items-center mt-4">
-            {successUpdateProfile ? (
-              <span className="text-lg font-semibold my-2 text-green-500">
-                Your profile has been updated !
-              </span>
-            ) : null}
-            <button
-              type="submit"
-              className="w-48 py-2 text-lg text-white rounded bg-blue-500 hover:bg-blue-700 transition duration-500 ease-out"
-            >
-              Update Profile
-            </button>
-          </div>
-        </form>
-      </div>
+      <UpdateProfile />
     </main>
   );
 };
 
-const mapStateToProps = (state) => {
-  return {
-    profile: state.firebase.profile,
-    userId: state.firebase.auth.uid,
-    successUpdateProfile: state.auth.successUpdateProfile,
-  };
-};
-
 const mapDispatchToProps = (dispatch) => {
   return {
-    updateProfile: (userId, newData) =>
-      dispatch(updateProfile(userId, newData)),
     cleanupUpdateMessage: () => dispatch({ type: 'CLEANUP_UPDATE_MESSAGE' }),
-    cleanupNewImageUrl: () => dispatch({ type: 'CLEANUP_NEW_IMAGE_URL' }),
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(ProfileInfo);
+export default connect(null, mapDispatchToProps)(ProfileInfo);
