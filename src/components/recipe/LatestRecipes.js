42a1be5a13f70ab441ece2ada6dd337582f8732b
andrealfian420 <andrealfian69699@gmail.com>
2020-10-18 16:28:11 +0700

    Fetch recipes data from firestore to latest recipe card component

--- src/components/recipe/LatestRecipes.js
+++ src/components/recipe/LatestRecipes.js
@@ -1,16 +1,38 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { firestoreConnect } from 'react-redux-firebase';
 import LatestRecipesCard from './LatestRecipesCard';
 
-const LatestRecipes = () => {
+const LatestRecipes = ({ recipes }) => {
   return (
     <div>
       <h4 className="font-bold mt-12 pb-2 border-b-4 border-gray-200">
         Latest recipes
       </h4>
 
-      <LatestRecipesCard />
+      {recipes ? (
+        <LatestRecipesCard recipes={recipes} />
+      ) : (
+        <div className="w-full text-center my-5">
+          <span className=" text-md text-red-700">
+            No recipe has been created
+          </span>
+        </div>
+      )}
     </div>
   );
 };
 
-export default LatestRecipes;
+const mapStateToProps = (state) => {
+  return {
+    recipes: state.firestore.ordered.recipes,
+  };
+};
+
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([
+    { collection: 'recipes', orderBy: ['createdAt', 'desc'], limit: 6 },
+  ])
+)(LatestRecipes);
--- src/components/recipe/LatestRecipesCard.js
+++ src/components/recipe/LatestRecipesCard.js
@@ -1,40 +1,52 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import dummyImg from '../../images/seblak.jpg';
 
-const LatestRecipesCard = () => {
+const LatestRecipesCard = ({ recipes }) => {
+  const recipesCard = recipes.map((recipe) => {
+    return (
+      <>
+        {/* Card */}
+        <div className="card hover:shadow-lg">
+          <img src={recipe.image} alt={recipe.title} className="card-img" />
+          <div className="m-4">
+            <Link
+              to={`/recipe/${recipe.slug}`}
+              className="font-bold hover:underline"
+            >
+              {recipe.title}
+            </Link>
+            <span className="block text-gray-500 text-sm">
+              Recipe by {''}
+              <a
+                href={`/profile/${recipe.authorId}`}
+                className="text-blue-700 hover:underline"
+              >
+                {recipe.authorFullName}
+              </a>
+            </span>
+          </div>
+          <div className="card-badge">
+            <svg
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="clock w-6 h-6 inline-block"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+            <span> {recipe.duration}</span>
+          </div>
+        </div>
+        {/* End of Card */}
+      </>
+    );
+  });
   return (
     <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-5">
-      {/* Card */}
-      <div className="card hover:shadow-lg">
-        <img src={dummyImg} alt="Dummy" className="card-img" />
-        <div className="m-4">
-          <Link to="/recipe/1" className="font-bold hover:underline">
-            Seblak Seafood
-          </Link>
-          <span className="block text-gray-500 text-sm">
-            Recipe by {''}
-            <a href="!#" className="text-blue-700 hover:underline">
-              Hakim Ziyech
-            </a>
-          </span>
-        </div>
-        <div className="card-badge">
-          <svg
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            className="clock w-6 h-6 inline-block"
-          >
-            <path
-              fillRule="evenodd"
-              d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-          <span> 25 Mins</span>
-        </div>
-      </div>
-      {/* End of Card */}
+      {recipesCard}
     </div>
   );
 };
