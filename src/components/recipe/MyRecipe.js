2edce1c964afcd219783263117e50f79ce58ea5a
andrealfian420 <andrealfian69699@gmail.com>
2020-10-24 15:17:36 +0700

    Fetch recipe data to profile component

--- src/components/recipe/MyRecipe.js
+++ src/components/recipe/MyRecipe.js
@@ -1,82 +1,52 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import dummyImg from '../../images/seblak.jpg';
 
-const MyRecipe = () => {
+const MyRecipe = ({ myRecipes }) => {
+  const recipeCards = myRecipes.map((recipe) => {
+    return (
+      <React.Fragment key={recipe.id}>
+        {/* Card */}
+        <div className="card hover:shadow-lg">
+          <img src={recipe.image} alt={recipe.title} className="card-img" />
+          <div className="m-4 mb-2 flex flex-col">
+            <Link
+              to={`/recipe/${recipe.slug}`}
+              className="font-bold hover:underline"
+            >
+              {recipe.title}
+            </Link>
+            <div className="mt-1">
+              <span className="text-white bg-green-600 hover:bg-green-500 cursor-pointer rounded-sm p-1 mr-1">
+                Update
+              </span>
+              <span className="text-white bg-red-600 hover:bg-red-500 cursor-pointer rounded-sm p-1">
+                Delete
+              </span>
+            </div>
+          </div>
+          <div className="card-badge">
+            <svg
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="clock w-6 h-6 inline-block"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+            <span> {recipe.duration}</span>
+          </div>
+        </div>
+        {/* End of Card */}
+      </React.Fragment>
+    );
+  });
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-5">
-      {/* Card */}
-      <div className="card hover:shadow-lg">
-        <img src={dummyImg} alt="Dummy" className="card-img" />
-        <div className="m-4 mb-2">
-          <Link to="/recipe/1" className="font-bold hover:underline">
-            Seblak Seafood
-          </Link>
-        </div>
-        <div className="card-badge">
-          <svg
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            className="clock w-6 h-6 inline-block"
-          >
-            <path
-              fillRule="evenodd"
-              d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-          <span> 25 Mins</span>
-        </div>
-      </div>
-      {/* End of Card */}
-      {/* Card */}
-      <div className="card hover:shadow-lg">
-        <img src={dummyImg} alt="Dummy" className="card-img" />
-        <div className="m-4 mb-2">
-          <Link to="/recipe/1" className="font-bold hover:underline">
-            Seblak Seafood
-          </Link>
-        </div>
-        <div className="card-badge">
-          <svg
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            className="clock w-6 h-6 inline-block"
-          >
-            <path
-              fillRule="evenodd"
-              d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-          <span> 25 Mins</span>
-        </div>
-      </div>
-      {/* End of Card */}
-      {/* Card */}
-      <div className="card hover:shadow-lg">
-        <img src={dummyImg} alt="Dummy" className="card-img" />
-        <div className="m-4 mb-2">
-          <Link to="/recipe/1" className="font-bold hover:underline">
-            Seblak Seafood
-          </Link>
-        </div>
-        <div className="card-badge">
-          <svg
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            className="clock w-6 h-6 inline-block"
-          >
-            <path
-              fillRule="evenodd"
-              d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-          <span> 25 Mins</span>
-        </div>
-      </div>
-      {/* End of Card */}
+      {recipeCards}
     </div>
   );
 };
--- src/components/user/Profile.js
+++ src/components/user/Profile.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { firestoreConnect } from 'react-redux-firebase';
 import UserProfileNavbar from '../layout/UserProfileNavbar';
 import UserProfileSection from './UserProfileSection';
 import MyRecipe from '../recipe/MyRecipe';
@@ -9,7 +11,7 @@ const Profile = (props) => {
     document.title = 'Profile';
   });
 
-  const { profile } = props;
+  const { profile, myRecipes } = props;
 
   return (
     <main className="px-16 py-6 bg-gray-100 md:col-span-10">
@@ -21,16 +23,38 @@ const Profile = (props) => {
           My Recipes
         </h1>
 
-        <MyRecipe />
+        {myRecipes ? (
+          <MyRecipe myRecipes={myRecipes} />
+        ) : (
+          <div className="text-red-600 text-center md:text-left mt-2 md:mt-3">
+            <span>You haven't created any recipe</span>
+          </div>
+        )}
       </div>
     </main>
   );
 };
 
 const mapStateToProps = (state) => {
+  const myRecipes = state?.firestore?.ordered?.myRecipes;
+
   return {
     profile: state.firebase.profile,
+    userId: state.firebase.auth.uid,
+    myRecipes,
   };
 };
 
-export default connect(mapStateToProps)(Profile);
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect((props) => {
+    return [
+      {
+        collection: 'recipes',
+        where: ['authorId', '==', props.userId],
+        orderBy: ['createdAt', 'desc'],
+        storeAs: 'myRecipes',
+      },
+    ];
+  })
+)(Profile);
