50b7d025dd0a5fd6fabf05af0584558c78d48952
andrealfian420 <andrealfian69699@gmail.com>
2020-10-24 14:40:49 +0700

    Change the behaviour of the star button if user not logged in

--- src/components/recipe/RecipeDetail.js
+++ src/components/recipe/RecipeDetail.js
@@ -92,18 +92,22 @@ const RecipeDetail = (props) => {
         />
       </div>
 
-      <div className="flex justify-center items-center mt-6">
-        <svg
-          viewBox="0 0 20 20"
-          fill={!starGiven ? 'currentColor' : '#fae13c'}
-          className="star w-12 h-12 cursor-pointer"
-          onClick={handleStarClick}
-        >
-          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-        </svg>
+      {auth?.uid ? (
+        <div className="flex justify-center items-center mt-6">
+          <svg
+            viewBox="0 0 20 20"
+            fill={!starGiven ? 'currentColor' : '#fae13c'}
+            className="star w-12 h-12 cursor-pointer"
+            onClick={handleStarClick}
+          >
+            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+          </svg>
 
-        <span>Give this recipe a star !</span>
-      </div>
+          <span>
+            {isStarGiven ? 'Star given !' : 'Give this recipe a star !'}
+          </span>
+        </div>
+      ) : null}
 
       <div className="mt-6 text-center md:text-left text-lg">
         <Link to="/" className="hover:underline">
